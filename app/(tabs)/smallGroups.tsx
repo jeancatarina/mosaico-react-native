58410054a7005e9cfa3cf4940679816eb993e364
jean catarina <jcc_ptb@hotmail.com>
2024-05-06 18:07:02 -0300

    feat: create calendar and map

--- app/(tabs)/smallGroups.tsx
+++ app/(tabs)/smallGroups.tsx
@@ -2,12 +2,24 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View } from '../../components/Themed';
 import { Strings } from '../../constants/Strings';
+import MapView from 'react-native-maps';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{Strings.smallGroups}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+	  <MapView
+	  	style={styles.map}
+		initialRegion={{
+			latitude: -26.281970,
+			longitude: -48.863880,
+			latitudeDelta: 0.09,
+			longitudeDelta: 0.04,
+		}}
+		showsUserLocation={true}
+		showsMyLocationButton={true}
+		></MapView>
+
     </View>
   );
 }
@@ -16,7 +28,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
@@ -27,4 +38,11 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  map: {
+	position:'absolute',
+top:0,
+bottom:0,
+left:0,
+right:0,
+  }
 });
